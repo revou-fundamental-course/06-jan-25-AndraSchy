d6ae20824bcd0c0c344bbd8b346c2343187178bb
Andra Aprilian <andraaprilian05@gmail.com>
2025-01-15 19:37:31 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+
+function formValidation() {
+
+    let nameInput = document.getElementById("tap").value;
+    console.log(nameInput)
+}
+
+document.getElementById("tap").addEventListener("click", formValidation);
